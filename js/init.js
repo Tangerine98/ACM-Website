3aae94819658114138eddf23919003f21d3fdf5d
Tangerine98 <menonnandini98@gmail.com>
2019-03-29 01:11:37 +0530

    Almost complete

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.materialboxed').materialbox();
+    $('.collapsible').collapsible();
+    $('.carousel').carousel();
+    //$('.pushpin').pushpin();
+    //$('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
